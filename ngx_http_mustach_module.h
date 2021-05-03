dc0024c3ade72853a114747c5792ec219720642a
RekGRpth <rekgrpth@gmail.com>
2021-05-03 09:10:56 +0500

    up

--- ngx_http_mustach_module.h
+++ ngx_http_mustach_module.h
@@ -1,3 +1,6 @@
+#ifndef _NGX_HTTP_MUSTACH_MODULE_H
+#define _NGX_HTTP_MUSTACH_MODULE_H
+
 #include <nginx.h>
 #include <ngx_http.h>
 #include <stddef.h>
@@ -6,3 +9,5 @@
 int ngx_http_mustach_process_cjson(ngx_http_request_t *r, const char *template, size_t length, const char *value, size_t buffer_length, FILE *file);
 int ngx_http_mustach_process_jansson(ngx_http_request_t *r, const char *template, size_t length, const char *buffer, size_t buflen, FILE *file);
 int ngx_http_mustach_process_json_c(ngx_http_request_t *r, const char *template, size_t length, const char *str, size_t len, FILE *file);
+
+#endif // _NGX_HTTP_MUSTACH_MODULE_H
